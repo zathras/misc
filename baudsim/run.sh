6a874a4a19a8da11d072bcf878078ee08cafc544
Bill Foote <billf@jovial.com>
2019-04-05 17:03:38 -0700

    Update run.sh

--- baudsim/run.sh
+++ baudsim/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# 
+#  Note:  If you're on OSX and you need stdbuf, you can get a quick and
+#         dirty version from https://github.com/tcreech/stdbuf-osx.git .
+#         Or you can install gnu coreutils, which most people seem to do
+#         using homebrew, which uses ruby.
+
 if [ $# != 1 ] ; then
     echo "Defaulting to 1200 baud."
     baud=1200
 else
     baud=$1
 fi
-exec stdbuf -o0 bash -i |& stdbuf -i0 -o0 -e0 ./baudsim $baud
+exec stdbuf -o0 bash -i 2>&1 | stdbuf -i0 -o0 -e0 ./baudsim $baud
