3ffec0d70d6bce93d697185f7eeab12b777fd3e9
Bill Foote <billf@jovial.com>
2020-02-22 18:25:43 -0800

    Fix up imports

--- maven_import/src/main/kotlin/Main.kt
+++ maven_import/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 
-import java.sql.PreparedStatement
 import com.jovial.db9010.*
-import java.util.logging.*
+import java.util.logging.Logger
+import java.util.logging.Level
+import java.util.logging.LogRecord
+import java.util.logging.Handler
 
 object People : Table("People") {
 
