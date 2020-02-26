6e79523a5d0d01498c02623840658ace2773754b
Bill Foote <billf@jovial.com>
2020-02-25 19:31:17 -0800

    Added a bit of design glitter.

--- playground/stream_server/src/main/kotlin/UsingCoroutines.kt
+++ playground/stream_server/src/main/kotlin/UsingCoroutines.kt
@@ -6,7 +6,6 @@ import java.nio.channels.AsynchronousServerSocketChannel
 import java.nio.channels.AsynchronousSocketChannel
 import java.nio.channels.CompletionHandler
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Future
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -30,59 +29,89 @@ object UsingCoroutines {
         ssChannel.bind(InetSocketAddress(7777))
         while (true) {
             val channel : AsynchronousSocketChannel = suspending { ssChannel.accept(null, it) }
-            val readBuffer = ByteBuffer.allocate(4)
-            val writeBuffer = ByteBuffer.allocate(5)
+            val output = OutputAdapter(channel, 5)
+            val input = InputAdapter(channel, 4)
             println(channel)
             async {
                 val line = StringBuffer()
                 while (true) {
-                    val bytesRead: Int = suspending { channel.read(readBuffer, null, it) }
-                    if (bytesRead == -1) {
+                    val chi = input.getchar()
+                    if (chi == -1) {
                         break
                     }
-                    readBuffer.flip()
-                    while (readBuffer.hasRemaining()) {
-                        val ch = readBuffer.get().toChar()
-                        println("Read $ch")
-                        line.append(ch)
-                        if (ch == '\r' || ch == '\n') {
-                            writeReversed(line, writeBuffer, channel)
-                        }
+                    val ch = chi.toChar()
+                    println("Read $ch")
+                    line.append(ch)
+                    if (ch == '\r' || ch == '\n') {
+                        writeReversed(line, output)
                     }
-                    readBuffer.clear()
                 }
+                println("Done with $channel")
             }
         }
     }
 
-    suspend fun writeReversed(line: StringBuffer, writeBuffer: ByteBuffer, channel: AsynchronousSocketChannel) {
+    suspend fun writeReversed(line: StringBuffer, writer: OutputAdapter) {
         while (line.isNotEmpty()) {
-            while (line.isNotEmpty() && writeBuffer.hasRemaining()) {
-                val i = if (line.length == 1) 0 else line.lastIndex - 1
-                val ch = line.get(i)        // avoid EOLN until end
-                line.deleteCharAt(i)
-                println("writing $ch")
-                writeBuffer.put(ch.toByte())
-            }
-            writeBuffer.flip()
-            while (writeBuffer.hasRemaining()) {
-                val result : Int = suspending { channel.write(writeBuffer, null, it) }
-                if (result == -1) {
-                    channel.close()
-                    break
-                }
-                if (!writeBuffer.hasRemaining()) {
-                    break
-                }
-            }
-            writeBuffer.clear()
+            val i = if (line.length == 1) 0 else line.lastIndex - 1
+            val ch = line.get(i)        // avoid EOLN until end
+            line.deleteCharAt(i)
+            println("writing $ch")
+            writer.write(ch.toByte())
         }
+        writer.flush()
     }
 }
 
-/**
- * I"m sure this exists in a standard library somewhere
- */
+class InputAdapter(val channel: AsynchronousSocketChannel, bufferSize: Int) {
+    private val buffer = ByteBuffer.allocate(bufferSize)
+
+    init {
+        buffer.flip()
+    }
+
+    suspend fun getchar() : Int {
+        if (!buffer.hasRemaining()) {
+            buffer.clear()
+            val bytesRead: Int = suspending { channel.read(buffer, null, it) }
+            buffer.flip()
+            if (bytesRead == -1) {
+                return -1
+            }
+        }
+        return buffer.get().toInt()
+    }
+}
+
+class OutputAdapter(val channel: AsynchronousSocketChannel, bufferSize: Int) {
+    private val buffer = ByteBuffer.allocate(bufferSize)
+
+    suspend fun write(b: Byte) {
+        if (!buffer.hasRemaining()) {
+            flush()
+        }
+        buffer.put(b)
+    }
+
+    suspend fun flush() {
+        buffer.flip()
+        while (buffer.hasRemaining()) {
+            val result : Int = suspending { channel.write(buffer, null, it) }
+            if (result == -1) {
+                channel.close()
+                break
+            }
+        }
+        buffer.clear()
+    }
+}
+
+
+/*******************************************************************
+ * Some glue between futures and coroutines.  I'm sure more
+ * generic versions of these exist in a standard library somewhere.
+ *******************************************************************/
+
 suspend fun<R> suspending(body: (h: CompletionHandler<R?, Unit?>) -> Unit) : R {
     val result = CompletableFuture<R>()
     body(object: CompletionHandler<R?, Unit?> {
@@ -106,9 +135,6 @@ suspend fun<R> suspending(body: (h: CompletionHandler<R?, Unit?>) -> Unit) : R {
     return result.await()
 }
 
-/**
- * I"m sure this exists in a standard library somewhere
- */
 suspend fun <T> CompletableFuture<T>.await(): T =
     suspendCoroutine<T> { cont: Continuation<T> ->
         whenComplete { result, exception ->
