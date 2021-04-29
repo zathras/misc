dac2dfb942e6f053bbe58a73a06b5ddf613e31d1
Bill Foote <billf@londo.local>
2021-04-29 12:59:34 -0700

    Got build to work on MacOSXI

--- scurses/unix/build.osx.sh
+++ scurses/unix/build.osx.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -x
-export JDK=/Library/java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home
 rm -rf ../lib
 mkdir ../lib
 cd ../src
@@ -7,4 +6,11 @@ javah scurses.RawCurses
 rm -f scurses/scurses_RawCurses.h
 mv scurses_RawCurses.h scurses
 cd scurses
-gcc -I$JDK/include -I$JDK/include/darwin -shared -dynamiclib -o ~/lib/libscurses.jnilib -lncurses *.c
+
+# Build for both Arm and Intel, since we might be on an M1 chip computer
+# running under Rosetta.  Or not.
+#
+# This is a really long way to go to get some ANSI terminal sequences out of
+# the curses library!  Oh well.
+
+cc -arch x86_64 -arch arm64 -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin -shared -dynamiclib -o ~/lib/libscurses.jnilib -lncurses *.c
